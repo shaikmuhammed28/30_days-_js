53533bb2635f3b5678a5a4d9ad4699f7eccd81a6
shaik <shaikshadoz3@gmail.com>
2026-02-05 17:37:23 +0530

    day5

---
+++ day5.js
@@ -0,0 +1,14 @@
+let out ="";
+
+for (let i=1;i<=10;i++){
+    for(let j=0;j<i;j++){
+        if(i%2==0){
+            out = out +'* ';
+           } else{
+            out = out +'* ';
+        }    
+        }
+         out = out +"\n";
+    }
+   
+console.log(out)
