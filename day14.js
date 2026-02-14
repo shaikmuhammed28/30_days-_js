d0e224362b657099d541d27531a4012fb150f748
shaik muhammed <shaikmdr28@gmail.com>
2026-02-14 23:22:05 +0530

    day14

---
+++ day14.js
@@ -0,0 +1,13 @@
+
+
+//count and string own code
+for (let num=0;num<=10;num++){
+     if(num==10){
+        console.log(num +"Blast off");
+    }else{
+        console.log(num +"knock down");
+    }
+
+}
+
+   
