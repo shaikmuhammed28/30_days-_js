ddc989a32cfe3508d60bdf30e97cfdacbe68c995
shaik <shaikshadoz3@gmail.com>
2026-02-18 20:19:01 +0530

    days18_array

---
+++ day18.js
@@ -0,0 +1,14 @@
+
+// let fruits1 =  new array();
+let namea = [];
+
+let fruits = ["Apple", ",cena", "Plum","arjun","brock","codey","deadpool"];
+    for(let i=0;i<fruits.length;i++){
+        if(fruits[i][0].toLowerCase() === "a")
+            namea.push(fruits[i])
+        console.log(namea)
+    }
+        
+
+// console.log(veges[1].name);
+// console.log(fruits)
